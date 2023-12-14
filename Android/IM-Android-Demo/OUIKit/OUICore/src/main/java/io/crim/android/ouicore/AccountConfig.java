e9d685bbb15cd66c0915e3a43f2fbe40afe3921d
jiaweizeng <597800295@qq.com>
2023-12-14 17:53:32 +0800

    首次提交

---
+++ Android/IM-Android-Demo/OUIKit/OUICore/src/main/java/io/crim/android/ouicore/AccountConfig.java
@@ -0,0 +1,10 @@
+package io.crim.android.ouicore;
+
+/**
+ * Created by zjw on 2023/12/5.
+ */
+public class AccountConfig {
+    public static final String APP_ID = "";
+    public static final String APP_SECRET = "";
+}
+
